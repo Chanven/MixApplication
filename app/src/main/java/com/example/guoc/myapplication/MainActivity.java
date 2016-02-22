3c8322b953a9502f03feb5d304bec5a09cbb06be
Chanven <chanven0127@gmail.com>
2016-02-22 16:17:44 +0800

    init commit

---
+++ app/src/main/java/com/example/guoc/myapplication/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.guoc.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mTablayout;
+    private ViewPager mViewPager;
+    private String[] mTitles = new String[]{"节日短信", "发送记录", "个人设置"};
+    private List<Fragment> mFragments;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        //初始化View
+        mTablayout = (TabLayout) findViewById(R.id.id_tab_layout);
+        mViewPager = (ViewPager) findViewById(R.id.id_view_pager);
+
+        //初始化List<Fragment>
+        mFragments = new ArrayList<>();
+        Fragment mFestivalFragment = new TestFragment();
+        Fragment mRecordFragment = new TestFragment();
+        Fragment myFragment = new TestFragment();
+        mFragments.add(mFestivalFragment);
+        mFragments.add(mRecordFragment);
+        mFragments.add(myFragment);
+
+        //给ViewPage设置Adapter
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return mFragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mFragments.size();
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mTitles[position];
+            }
+        });
+
+        mTablayout.setupWithViewPager(mViewPager);
+    }
+}
---
+++ app/src/main/java/com/example/guoc/myapplication/RCircleview.java
@@ -0,0 +1,116 @@
+package com.example.guoc.myapplication;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+
+/**
+ *
+ * Created by Chanven on 2016/1/15.
+ */
+public class RCircleview extends View{
+
+    private int width;
+    private int height;
+
+    private static final String TAG = "RCircleView";
+
+    public RCircleview(Context context) {
+        super(context);
+    }
+
+    public RCircleview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    private void init() {
+        WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        width = windowManager.getDefaultDisplay().getWidth();
+        height = windowManager.getDefaultDisplay().getHeight();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = getMeasuredWidth();
+        int height = getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+        Log.i(TAG, "width--" + width + "--height--" + height);
+        setMeasuredDimension(getMeasuredWidth(), MeasureSpec.getSize(heightMeasureSpec));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Log.i(TAG, "onDraw");
+        Paint paint = new Paint();
+//        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+        RectF rectF = new RectF(0,0,300,300);
+        canvas.drawArc(rectF, 0, 300, true, paint);
+
+//        paint = new Paint();
+//        canvas.drawCircle(200,200,50,paint);
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+    }
+
+    /**
+     * Determines the width of this view
+     * @param measureSpec A measureSpec packed into an int
+     * @return The width of the view, honoring constraints from measureSpec
+     */
+    private int measureWidth(int measureSpec) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.EXACTLY) {
+            // We were told how big to be
+            result = specSize;
+        } else {
+            // Measure the text
+//            result = (int) mTextPaint.measureText(mText) + getPaddingLeft()
+//                    + getPaddingRight();
+            if (specMode == MeasureSpec.AT_MOST) {
+                // Respect AT_MOST value if that was what is called for by measureSpec
+                result = Math.min(result, specSize);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Determines the height of this view
+     * @param measureSpec A measureSpec packed into an int
+     * @return The height of the view, honoring constraints from measureSpec
+     */
+    private int measureHeight(int measureSpec) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+//        mAscent = (int) mTextPaint.ascent();
+        if (specMode == MeasureSpec.EXACTLY) {
+            // We were told how big to be
+            result = specSize;
+        } else {
+            // Measure the text (beware: ascent is a negative number)
+//            result = (int) (-mAscent + mTextPaint.descent()) + getPaddingTop()
+//                    + getPaddingBottom();
+            if (specMode == MeasureSpec.AT_MOST) {
+                // Respect AT_MOST value if that was what is called for by measureSpec
+                result = Math.min(result, specSize);
+            }
+        }
+        return result;
+    }
+}
